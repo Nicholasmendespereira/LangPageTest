207a5a6a64d3dd3df2e50d3f83d9b108b22a0f2d
Nicholas mendes <98132580+Nicholasmendespereira@users.noreply.github.com>
2023-04-15 22:37:18 -0300

    up

--- src/pages/FashionMen/index.jsx
+++ src/pages/FashionMen/index.jsx
@@ -1,11 +1,21 @@
 import api from "../../api/api";
 import "./style.js";
-import { Header, Main, Form, Agendado, SubAgendado } from "./style.js";
+import { useEffect, useRef } from "react";
+import { motion } from "framer-motion";
+
+import {
+  Header,
+  Main,
+  Form,
+  Agendado,
+  SubAgendado,
+  Carousel,
+  DetailsProcess,
+} from "./style.js";
 import { useState } from "react";
 import moment from "moment";
 import { formatShift, formatProcess } from "../../utilits";
 import { toast } from "react-toastify";
-import logo from "../../assets/logosemfundo.png";
 
 import {
   AiOutlineSearch,
@@ -25,45 +35,143 @@ import Button from "@mui/material/Button";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
-import { useEffect } from "react";
+import Dialog from "@mui/material/Dialog";
+import DialogTitle from "@mui/material/DialogTitle";
+import Stepper from "@mui/material/Stepper";
+import Step from "@mui/material/Step";
+import StepLabel from "@mui/material/StepLabel";
+import StepContent from "@mui/material/StepContent";
+import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
+
+import logo from "../../assets/logosemfundo.png";
+import img01 from "../../assets/img4.jpg";
+import img03 from "../../assets/paper03.jpg";
+import img04 from "../../assets/paper04.jpg";
+import img05 from "../../assets/paper05.jpg";
+import img06 from "../../assets/paper06.jpg";
+import img07 from "../../assets/paper07.jpg";
+import img08 from "../../assets/paper08.jpg";
+import img09 from "../../assets/paper09.jpg";
+import img10 from "../../assets/paper10.jpg";
 
 function FashionMen() {
-  const [formData, setFormData] = useState({});
+  const carouselRef = useRef();
+  const images = [
+    img01,
+    img03,
+    img04,
+    img05,
+    img06,
+    img07,
+    img08,
+    img09,
+    img10,
+  ];
+  const steps = [
+    {
+      label: "Oque é Hidratação capilar?",
+      description: `A hidratação capilar é responsável por repor a água e os nutrientes que os fios perdem diariamente. Com a exposição ao sol e frio, uso de secador e chapinha, além de química no cabelo, deixam os fios ressecados e opacos..`,
+    },
+    {
+      label: "Oque é Selagem capilar?",
+      description:
+        "É um procedimento que fecha as cutículas do cabelo, reduz volume e deixa os fios alinhados. A selagem capilar é uma ótima opção principalmente para quem tem cabelo liso natural e quer diminuir o volume das madeixas. Também faz toda a diferença para quem alisa os fios e quer prolongar o efeito da escova.",
+    },
+    {
+      label: "Oque é Progressiva capilar?",
+      description: `A progressiva tem como objetivo o alisamento dos fios, por isso, ela também é identificada como um químico alisante. Os alisantes são produtos desenvolvidos para modificar a estrutura interna do fio, chegando até camadas profundas e alterando quimicamente sua estrutura.`,
+    },
+    {
+      label: "O que é uma escova capilar?",
+      description: `A escova progressiva é uma das técnicas mais usadas pelas mulheres que querem alisar o cabelo e deixar os fios bem alinhados. Ela é muito popular e pode ser usada em cabelos ondulados, cacheados e crespos, o que faz com que seja um tratamento muito versáti.`,
+    },
+    {
+      label: "O que e um corte de cabelo?",
+      description: `O Corte de cabelo é o que define o estilo visual dos cabelos e consequentemente, o estilo visual da pessoa. Acompanhar o design do corte, com a personalidade da pessoa, torna a mesma mais autentica e única, pois ela será representada por si mesma.`,
+    },
+  ];
 
-  // name: formData?.name,
-  // process: formData?.process,
-  // hour: formData?.hour,
-  const HandleSumit = async () => {
-    try {
-      const resp = await api({
-        method: "POST",
-        url: "/new-user",
-        data: {
-          name: formData?.name,
-          process: formData?.process,
-          hour: formData?.hour,
-          day: formData?.day,
-          shift: formData?.shift,
-        },
-        headers: {
-          "Content-Type": "application/json",
-        },
-        json: true,
-      });
-      toast.success({
-        position: toast.POSITION.TOP_RIGHT,
-        theme: "colored",
-      });
-    } catch (e) {
-      toast.error({
-        position: toast.POSITION.TOP_RIGHT,
-        theme: "colored",
-      });
-    }
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
   };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  const handleReset = () => {
+    setActiveStep(0);
+  };
+  const [width, setWidth] = useState(0);
+  const [image, setImage] = useState(img01);
+  const [formData, setFormData] = useState({});
+  const [openDialog, setOpenDialog] = useState(false);
+  const [activeStep, setActiveStep] = useState(0);
+
+  useEffect(() => {
+    setWidth(
+      carouselRef.current?.scrollWidth - carouselRef.current?.scrollWidth
+    );
+  }, []);
+  const handleClickOpen = () => {
+    setOpenDialog(true);
+  };
+
+  const handleClose = () => {
+    setOpenDialog(false);
+  };
+
+  const HandleSumit = (e) => {
+    // e.preventDefault();
+    // setOpen(true);
+    const message = `https://api.whatsapp.com/send?phone=5534996442120&text=Olá! Gostaria de marcar um horário. Meu nome é: *${
+      formData?.name
+    }*,quero fazer: *${formatProcess(formData?.process)?.label}*, as: *${
+      formData?.hour
+    }* da *${formatShift(formData?.shift)?.label}*, no dia: *${
+      formData?.day
+    }*!`;
+    window.open(message, "_blank");
+    toast.success({
+      position: toast.POSITION.TOP_RIGHT,
+      theme: "colored",
+    });
+  };
+  // carrousel
+
+  // const HandleSumit = async () => {
+  //   try {
+  //     const resp = await api({
+  //       method: "POST",
+  //       url: "/new-user",
+  //       data: {
+  //         name: formData?.name,
+  //         process: formData?.process,
+  //         hour: formData?.hour,
+  //         day: formData?.day,
+  //         shift: formData?.shift,
+  //       },
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //       },
+  //       json: true,
+  //     });
+  //     toast.success({
+  //       position: toast.POSITION.TOP_RIGHT,
+  //       theme: "colored",
+  //     });
+  //   } catch (e) {
+  //     toast.error({
+  //       position: toast.POSITION.TOP_RIGHT,
+  //       theme: "colored",
+  //     });
+  //   }
+  // };
+
   return (
     <>
-      <Main>
+      <Main backgroundImage={image}>
         <Header>
           <img src={logo} alt="logo" className="logo" />
           <div className="barVertical">.</div>
@@ -83,120 +191,184 @@ function FashionMen() {
           </ul>
         </Header>
       </Main>
-
-      <Form>
-        <Box
-          component="form"
-          sx={{
-            "& > :not(style)": { m: 1, width: "25ch" },
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-          noValidate
-          autoComplete="off"
+      <Carousel>
+        <motion.div
+          ref={carouselRef}
+          className="carousel"
+          whileTap={{ cursor: "grabbing" }}
         >
-          <TextField
-            style={{ width: "45.5%" }}
-            id="standard-basic"
-            label="Nome"
-            variant="standard"
-            className="input"
-            onChange={(e) => setFormData({ ...formData, name: e.target.value })}
-          />
-          <br />
-          <FormControl variant="standard" sx={{ m: 1, minWidth: 260 }}>
-            <InputLabel id="demo-simple-select-standard-label">
-              Procedimento
-            </InputLabel>
-            <Select
-              labelId="demo-simple-select-standard-label"
-              id="demo-simple-select-standard"
-              value={formData?.process}
-              onChange={(e) =>
-                setFormData({ ...formData, process: e.target.value })
-              }
-              label="Procedimentos"
-            >
-              <MenuItem value="">
-                <em>None</em>
-              </MenuItem>
-              <MenuItem value="brush">Escova</MenuItem>
-              <MenuItem value="cut">Corte</MenuItem>
-              <MenuItem value="progressive">Progressiva</MenuItem>
-            </Select>
-          </FormControl>
-          <TextField
-            style={{ width: "45.5%" }}
-            id="standard-basic"
-            label="Horário"
-            placeholder="horas"
-            variant="standard"
-            className="input"
-            onChange={(e) => setFormData({ ...formData, hour: e.target.value })}
-          />
-          <FormControl
-            variant="standard"
-            sx={{ m: 1, minWidth: 260 }}
-            style={{ marginTop: "2rem", marginBottom: "2.5rem" }}
+          <motion.div
+            className="inner"
+            drag="x"
+            dragConstraints={{ right: 0, left: -width }}
+            initial={{ x: 500 }}
+            animate={{ x: 2 }}
+            transition={{ duration: 10 }}
           >
-            <InputLabel id="demo-simple-select-standard-label">
-              Manhã, Tarde ou Noite
-            </InputLabel>
-            <Select
-              labelId="demo-simple-select-standard-label"
-              id="demo-simple-select-standard"
-              value={formData?.shift}
-              onChange={(e) =>
-                setFormData({ ...formData, shift: e.target.value })
-              }
-              label="Manhã, Tarde ou Noite"
+            {images.map((image) => (
+              <motion.div className="item">
+                <img src={image} alt="text-alt" />
+                <Button
+                  onClick={() => {
+                    setImage(image);
+                  }}
+                  style={{ borderRadius: "50%", width: "10px" }}
+                >
+                  O
+                </Button>
+              </motion.div>
+            ))}
+          </motion.div>
+        </motion.div>
+      </Carousel>
+
+      <Button
+        variant="outlined"
+        onClick={handleClickOpen}
+        style={{
+          border: "1px solid rgba(170,118,74, 0.5)",
+          color: "rgb(170,118,74)",
+          width: "15rem",
+          margin: "2rem 0",
+        }}
+      >
+        Agendar
+      </Button>
+      <Dialog
+        open={openDialog}
+        onClose={handleClose}
+        style={{ background: "rgba(170,118,74, 0.5)" }}
+      >
+        <DialogTitle style={{ textAlign: "center", color: "gray " }}>
+          {"Vamos marcar um horário?"}
+        </DialogTitle>
+        <div style={{ width: "35rem" }}>
+          <Form>
+            <Box
+              component="form"
+              sx={{
+                "& > :not(style)": { m: 1, width: "25ch" },
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+              noValidate
+              autoComplete="off"
             >
-              <MenuItem value="">
-                <em>None</em>
-              </MenuItem>
-              <MenuItem value="morning">Manhã</MenuItem>
-              <MenuItem value="afternoon">Tarde</MenuItem>
-              <MenuItem value="night">Noite</MenuItem>
-            </Select>
-          </FormControl>
-          <div
-            style={{
-              display: "flex",
-              width: "100%",
-              marginTop: "3rem",
-              alignItems: "center",
-              justifyContent: "center",
-              margin: "0 auto",
-            }}
-          >
-            {/* // day: moment(e.target.value).format("DD/MM/YYYY"), */}
-            <LocalizationProvider dateAdapter={AdapterDayjs}>
-              <DatePicker 
-                onChange={(e) => setFormData({...formData, day: e.target.value})}
+              <TextField
+                style={{ width: "67%" }}
+                id="standard-basic"
+                label="Nome"
+                variant="standard"
+                className="input"
+                onChange={(e) =>
+                  setFormData({ ...formData, name: e.target.value })
+                }
               />
-            </LocalizationProvider>
-          </div>
-          {console.log(formData?.day)}
-          <h2>{formData?.day}</h2>
-          <Button
-            style={{
-              marginTop: "2.5rem",
-              border: "1px solid rgba(170,118,74, 0.5)",
-              color: "rgb(170,118,74)",
-            }}
-            // disabled={}
-            variant="outlined"
-            onClick={() => {
-              HandleSumit();
-            }}
-          >
-            Agendar
-          </Button>
-        </Box>
-      </Form>
-      <Agendado>
+              <br />
+              <FormControl variant="standard" sx={{ m: 1, minWidth: 260 }}>
+                <InputLabel id="demo-simple-select-standard-label">
+                  Procedimento
+                </InputLabel>
+                <Select
+                  labelId="demo-simple-select-standard-label"
+                  id="demo-simple-select-standard"
+                  value={formData?.process}
+                  onChange={(e) =>
+                    setFormData({ ...formData, process: e.target.value })
+                  }
+                  label="Procedimentos"
+                >
+                  <MenuItem value="">
+                    <em>None</em>
+                  </MenuItem>
+                  <MenuItem value="brush">Escova</MenuItem>
+                  <MenuItem value="cut">Corte</MenuItem>
+                  <MenuItem value="progressive">Progressiva</MenuItem>
+                </Select>
+              </FormControl>
+              <TextField
+                style={{ width: "67%" }}
+                id="standard-basic"
+                label="Horário"
+                placeholder="horas"
+                variant="standard"
+                className="input"
+                onChange={(e) =>
+                  setFormData({ ...formData, hour: e.target.value })
+                }
+              />
+              <FormControl
+                variant="standard"
+                sx={{ m: 1, minWidth: 260 }}
+                style={{ marginTop: "2rem", marginBottom: "2.5rem" }}
+              >
+                <InputLabel id="demo-simple-select-standard-label">
+                  Manhã, Tarde ou Noite
+                </InputLabel>
+                <Select
+                  labelId="demo-simple-select-standard-label"
+                  id="demo-simple-select-standard"
+                  value={formData?.shift}
+                  onChange={(e) =>
+                    setFormData({ ...formData, shift: e.target.value })
+                  }
+                  label="Manhã, Tarde ou Noite"
+                >
+                  <MenuItem value="">
+                    <em>None</em>
+                  </MenuItem>
+                  <MenuItem value="morning">Manhã</MenuItem>
+                  <MenuItem value="afternoon">Tarde</MenuItem>
+                  <MenuItem value="night">Noite</MenuItem>
+                </Select>
+              </FormControl>
+              <div
+                style={{
+                  display: "flex",
+                  width: "100%",
+                  marginTop: "3rem",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  margin: "0 auto",
+                }}
+              >
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                  <DatePicker
+                    format="DD/MM/YYYY"
+                    onChange={(e) =>
+                      setFormData({
+                        ...formData,
+                        day: e.format("DD/MM/YYYY"),
+                      })
+                    }
+                  />
+                </LocalizationProvider>
+              </div>
+              <Button
+                style={{
+                  marginTop: "2.5rem",
+                  border: "1px solid rgba(170,118,74, 0.5)",
+                  color: "rgb(170,118,74)",
+                }}
+                // disabled={}
+                variant="outlined"
+                onClick={() => {
+                  HandleSumit();
+                  handleClose();
+                }}
+              >
+                Agendar
+              </Button>
+            </Box>
+          </Form>
+        </div>
+        <Button onClick={handleClose} autoFocus style={{ color: "gray" }}>
+          Sair
+        </Button>
+      </Dialog>
+      <Agendado onClick={handleClickOpen}>
         <h3>
           Cliente: <br />
           <span>{formData?.name}</span>
@@ -209,13 +381,71 @@ function FashionMen() {
           Agendamento: <br />
           <span>
             <SubAgendado>
-              {formData?.hour} <small>horas</small>, <br />
-              <small>Dia</small>: {formData?.day} <br />
+              {formData?.hour} <small>horas,da</small>{" "}
               <small>{formatShift(formData?.shift)?.label}</small> <br />
+              <br />
+              <small>Dia</small>: {formData?.day} <br />
             </SubAgendado>
           </span>
         </h3>
       </Agendado>
+
+      <DetailsProcess>
+        <Box sx={{ maxWidth: 400 }}>
+          <Stepper activeStep={activeStep} orientation="vertical">
+            {steps.map((step, index) => (
+              <Step key={step.label} style={{ textAlign: "start" }}>
+                <StepLabel
+                  optional={
+                    index === 4 ? (
+                      <Typography variant="caption">Última</Typography>
+                    ) : null
+                  }
+                >
+                  {step.label}
+                </StepLabel>
+                <StepContent>
+                  <Typography>{step.description}</Typography>
+                  <Box sx={{ mb: 2 }}>
+                    <div>
+                      <Button
+                        variant="contained"
+                        onClick={handleNext}
+                        sx={{ mt: 1, mr: 1 }}
+                      >
+                        {index === steps.length - 1 ? "Final" : "Próxim."}
+                      </Button>
+                      <Button
+                        disabled={index === 0}
+                        onClick={handleBack}
+                        sx={{ mt: 1, mr: 1 }}
+                      >
+                        Voltar
+                      </Button>
+                    </div>
+                  </Box>
+                </StepContent>
+              </Step>
+            ))}
+          </Stepper>
+          {activeStep === steps.length && (
+            <Paper square elevation={0} sx={{ p: 3 }}>
+              <Typography>
+                Algumas das perguntas frenquentes - <br />{" "}
+                <span
+                  onClick={() => alert("Consulte nosso blog")}
+                  style={{ color: "rgb(170, 118, 74)", cursor: "pointer" }}
+                >
+                  Quer saber mais?
+                </span>
+              </Typography>
+              <Button onClick={handleReset} sx={{ mt: 1, mr: 1 }}>
+                Recomeçar
+              </Button>
+            </Paper>
+          )}
+        </Box>
+      </DetailsProcess>
     </>
   );
 }
--- src/pages/FashionMen/style.js
+++ src/pages/FashionMen/style.js
@@ -5,7 +5,7 @@ export const Header = styled.header`
 display: flex;
 align-items: center;
 height: 5rem;
-width: 100vw;
+width: 100%;
 .logo{
     width: 8%;
 }
@@ -45,20 +45,21 @@ color: white;
 }
 `;
 export const Main = styled.main`
-height: 100vh;
-width: 100vw;
-background-image:url(${background});
+height: 90vh;
+width: 100%;
+background-image:url(${props => props.backgroundImage});
 background-repeat: no-repeat;
 background-size: 100%;
+background-position: 50% 50%;
 opacity: 1;
 `;
 export const Div = styled.div`
-width: 100vw;
+width: 100%;
 height: 10vh;
 background-color: rgba(255,255,255,1);
 `;
 export const Form = styled.form`
-width: 40%;
+width: 70%;
 margin: 2rem auto;
 padding-bottom: 10px;
 border: 1px solid rgba(170,118,74, 0.5);
@@ -67,10 +68,32 @@ border: 1px solid rgba(170,118,74, 0.5);
     margin-top: 2rem;
 }
 `;
+export const Carousel = styled.div`
+/* max-width: 60%px; */
+.inner {
+    display: flex;
+}
+.item {
+    min-height:20%;
+    width: 400px;
+    padding:12px;
+}
+.item img{
+    width:100%;
+    height:90%;
+    border-radius: 12px;
+    pointer-events:none;
+}
+.carousel {
+    cursor: grab;
+    overflow: hidden;
+}
+`;
 export const Agendado = styled.div`
-width: 30%;
+font-family: 'Special Elite';
+width: 25%;
 height: 20%;
-border: 1px solid lightgrey;
+border: 1px dashed lightgrey;
 border-radius:20px;
 display: flex;
 flex-direction: column;
@@ -78,17 +101,30 @@ justify-content: center;
 align-items: center;
 margin: 0 auto;
 color: gray;
+padding: 1rem;
 h3 {
-    color: lightgray
+    color: lightgray;
+    margin: 0.5rem 0;
 }
 span {
+    font-family: 'Dancing Script';
     color: gray;
 }
 `;
 export const SubAgendado = styled.div`
+padding: 0.5rem;
 margin-top: 5%;
 width: 100%;
 height: 100%;
-border: 1px solid lightgrey;
+border: 1px dashed lightgrey;
 border-radius: 10px;
+`;
+export const DetailsProcess = styled.div`
+display: flex;
+align-items: center;
+justify-content: center;
+margin:2rem auto;
+width:100%;
+background-image: linear-gradient(to bottom, lightgray, rgba(255,255,255,0.2));
+height: 100vh;
 `;
