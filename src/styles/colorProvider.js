68537be07a0fe07ea1cc557f78aa51c81280d792
Nicholas mendes <98132580+Nicholasmendespereira@users.noreply.github.com>
2023-04-25 16:15:32 -0300

    ColorsGrays

--- src/styles/colorProvider.js
+++ src/styles/colorProvider.js
@@ -4,3 +4,11 @@ export const HardBrown = '#402019'
 export const Black = '#0D0000'
 export const GreenLigth = '#CAD9BF'
 export const HardLigth = '#486255'
+
+
+//TEMA GRAY
+export const lightGray = '#929daa'
+export const MediumGray = '#7e8287'
+export const HardGray = '#3b3c42'
+export const SemiBlack = '#191a1e'
+export const BlacBlack = '#0a0a0b'
